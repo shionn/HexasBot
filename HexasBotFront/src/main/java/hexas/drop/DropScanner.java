ceb08a20b3ad6276f0156b35bf76df133e6083b6
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-03-18 00:07:43 +0100

    Ajout favicon

--- HexasBotFront/src/main/java/hexas/drop/DropScanner.java
+++ HexasBotFront/src/main/java/hexas/drop/DropScanner.java
@@ -28,7 +28,7 @@ public class DropScanner implements Serializable {
 	private Iterator<Product> products;
 	private Map<String, Map<String, String>> cookiePerSites = new HashMap<>();
 
-	@Scheduled(fixedDelay = 8, timeUnit = TimeUnit.SECONDS)
+	@Scheduled(fixedDelay = 5, timeUnit = TimeUnit.SECONDS)
 	public void scanWithJsoop() {
 		if (products == null || !products.hasNext()) {
 			System.out.println("list all product to scan ");
