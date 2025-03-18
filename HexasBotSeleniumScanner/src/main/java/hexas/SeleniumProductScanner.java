8ec4e4aa76b14b2c8edf3363c4d33af92cef961d
shionn <shionn@gmail.com>
2025-03-18 10:43:30 +0100

    Selenium web scanner

--- HexasBotSeleniumScanner/src/main/java/hexas/SeleniumProductScanner.java
+++ HexasBotSeleniumScanner/src/main/java/hexas/SeleniumProductScanner.java
@@ -1,5 +1,9 @@
 package hexas;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.jsoup.Jsoup;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -19,19 +23,37 @@ public class SeleniumProductScanner {
 	private static void doParsing() {
 		FirefoxOptions options = new FirefoxOptions();
 		FirefoxDriver driver = new FirefoxDriver(options);
-		new SessionFactory().open().getMapper(ProductScanDao.class).list("javafx");
-		try {
-			Product product = Product
-					.builder()
-					.url("https://www.pccomponentes.fr/carte-graphique-carte-graphique-asus-tuf-gaming-amd-radeon-rx-9070-xt-oc-16-go-gddr6-fsr-4")
-					.build();
-			driver.get(product.getUrl());
-			PageParser parser = new PageParserRetreiver().resolve(product);
-			parser.parse(Jsoup.parse(driver.getPageSource()), product);
-		} catch (Exception e) {
-			System.out.println(e);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
+		try(SqlSession session = new SessionFactory().open()) {
+			List<Product> products = session.getMapper(ProductScanDao.class).list("selenium");
+			for (Product product : products) {
+				doScan(driver, product);
+			}
 		}
 		driver.close();
 
+	}
+
+	private static void doScan(FirefoxDriver driver, Product product) {
+		try {
+			System.out.println("scan " + product);
+			driver.get(product.getUrl());
+			String source = driver.getPageSource();
+//			if (source.contains("Cookies et choix publicitaires")) {
+//				driver.findElement(Bys.id("sp-cc-rejectall-link")).click();
+//			}
+//			if (source.contains("Rejeter les cookies")) {
+//				driver.findElement(Bys.text("Rejeter les cookies")).click();
+//			}
+//			if (source.contains("Do not accept")) {
+//				driver.findElement(Bys.text("Do not accept")).click();
+//			}
+			PageParser parser = new PageParserRetreiver().resolve(product);
+			parser.parse(Jsoup.parse(source), product);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
