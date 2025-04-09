d4fb85475dd944dedc811e8109fbe582f81238e4
Shionn (Kepka Ludovic) <shionn@gmail.com>
2025-04-09 15:49:14 +0200

    Mise à jour du scanner de group amazon

--- HexasBotWebParser/src/main/java/hexas/parser/AmazonPageParser.java
+++ HexasBotWebParser/src/main/java/hexas/parser/AmazonPageParser.java
@@ -5,7 +5,9 @@ import java.text.DecimalFormatSymbols;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
@@ -37,10 +39,29 @@ public class AmazonPageParser implements PageParser {
 			BigDecimal price = price(element, "span.a-price .a-offscreen");
 			String url = "https://www.amazon.fr/dp/" + element.attr("data-dib-asin");
 			String name = text(element, "h2");
-			if (name.contains(group.getMetaModel()) && element.text().contains("Ajouter au panier") && !name.contains("Memory PC")) {
+			if (isValidProduct(group, name) && isInStock(element) && !isIgnored(group, name)) {
 				new PriceUpdater().createOrUpdate(name, url, price, group.getVendor(), group);
 			}
 		});
+	}
+
+	private boolean isIgnored(Product group, String name) {
+		boolean ignored = false;
+		if (StringUtils.isNotBlank(group.getExcludePattern())) {
+			ignored = Pattern.compile(group.getExcludePattern()).matcher(name).find();
+		}
+		return ignored || name.contains("Memory PC") || name.contains("VIBOX");
+	}
+
+	private boolean isValidProduct(Product group, String name) {
+		if (StringUtils.isNotBlank(group.getIncludePattern())) {
+			return Pattern.compile(group.getIncludePattern()).matcher(name).find();
+		}
+		return name.contains(group.getMetaModel());
+	}
+
+	private boolean isInStock(Element element) {
+		return element.text().contains("Ajouter au panier");
 	}
 
 	@Override
