fb8fdf3a731e9c81364762e74dedb17cdd8288bb
Kepka Ludovic <shionn@gmail.com>
2025-08-07 17:32:56 +0200

    correction avec test justine

--- HexasBotDbConnector/src/main/java/hexas/db/dao/ProductsDao.java
+++ HexasBotDbConnector/src/main/java/hexas/db/dao/ProductsDao.java
@@ -22,7 +22,7 @@ public interface ProductsDao {
 	int update(Product product);
 
 	@Update("INSERT product (marque, meta_model, name, msrp, notify_price, notify_channel) "
-			+ "VALUES (#{marque}, #{name}, #{metaModel}, #{msrp}, #{notifyPrice}, #{notifyChannel})")
+			+ "VALUES (#{marque}, #{metaModel}, #{name}, #{msrp}, #{notifyPrice}, #{notifyChannel})")
 	int create(Product product);
 
 	@Delete("DELETE FROM product WHERE id = #{id}")
