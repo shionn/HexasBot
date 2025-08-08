aa727c15a92f1bd7284dd0c1fc8772fd1fb8e40a
Kepka Ludovic <shionn@gmail.com>
2025-08-09 00:07:39 +0200

    service web json humble bundle

---
+++ HexasBotFront/src/main/java/hexas/humblebundle/HumbleBundleController.java
@@ -0,0 +1,30 @@
+package hexas.humblebundle;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import hexas.db.dao.HumbleBundleDao;
+import hexas.db.dbo.Bundle;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(path = "/humble-bundle")
+public class HumbleBundleController {
+
+	private final SqlSession session;
+
+	@GetMapping(path = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	List<Bundle> getJson() {
+		List<Bundle> bundles = session.getMapper(HumbleBundleDao.class).list();
+		return bundles;
+	}
+
+}
