a10f4dc5e23ddab9c72cb7089f1f73e7f9f645d2
Kepka Ludovic <shionn@gmail.com>
2025-08-28 08:52:30 +0200

    mlise a jour structure BDD

--- HexasBotDbConnector/src/main/sql/structure.sql
+++ HexasBotDbConnector/src/main/sql/structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : ven. 08 août 2025 à 19:25
+-- Généré le : jeu. 28 août 2025 à 06:19
 -- Version du serveur :  10.5.15-MariaDB-0+deb11u1
 -- Version de PHP : 7.4.30
 
@@ -11,11 +11,28 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Base de données : `hexas`
 --
-CREATE DATABASE IF NOT EXISTS `hexas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `hexas`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `api_key`
+--
+
+CREATE TABLE `api_key` (
+  `id` int(11) NOT NULL,
+  `user` varchar(32) NOT NULL,
+  `value` varchar(512) NOT NULL,
+  `type` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -23,13 +40,12 @@ USE `hexas`;
 -- Structure de la table `bundle`
 --
 
-CREATE TABLE IF NOT EXISTS `bundle` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `bundle` (
+  `id` int(11) NOT NULL,
   `url` varchar(1024) NOT NULL,
   `name` varchar(128) NOT NULL,
   `end_date` datetime NOT NULL,
-  `notified` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+  `notified` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -38,12 +54,10 @@ CREATE TABLE IF NOT EXISTS `bundle` (
 -- Structure de la table `bundle_choice`
 --
 
-CREATE TABLE IF NOT EXISTS `bundle_choice` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `bundle_choice` (
+  `id` int(11) NOT NULL,
   `bundle` int(11) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `bundle_choice_to_bundle` (`bundle`)
+  `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -52,10 +66,9 @@ CREATE TABLE IF NOT EXISTS `bundle_choice` (
 -- Structure de la table `bundle_choice_game`
 --
 
-CREATE TABLE IF NOT EXISTS `bundle_choice_game` (
+CREATE TABLE `bundle_choice_game` (
   `choice` int(11) NOT NULL,
-  `game` varchar(256) NOT NULL,
-  PRIMARY KEY (`choice`,`game`)
+  `game` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -64,8 +77,8 @@ CREATE TABLE IF NOT EXISTS `bundle_choice_game` (
 -- Structure de la table `product`
 --
 
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
   `marque` varchar(32) NOT NULL,
   `meta_model` varchar(32) NOT NULL,
   `model` varchar(256) DEFAULT NULL,
@@ -80,10 +93,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `notify_channel` varchar(32) DEFAULT NULL,
   `scanner` varchar(32) DEFAULT NULL,
   `include_pattern` varchar(128) DEFAULT NULL,
-  `exclude_pattern` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `url` (`url`) USING HASH,
-  KEY `url_2` (`url`(768))
+  `exclude_pattern` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -92,16 +102,14 @@ CREATE TABLE IF NOT EXISTS `product` (
 -- Structure de la table `task`
 --
 
-CREATE TABLE IF NOT EXISTS `task` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
   `parent` int(11) DEFAULT NULL,
   `type` varchar(32) NOT NULL,
   `url` varchar(1024) NOT NULL,
   `product` int(11) NOT NULL,
   `include_pattern` varchar(32) DEFAULT NULL,
-  `exclude_pattern` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `product` (`product`)
+  `exclude_pattern` varchar(32) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -110,13 +118,97 @@ CREATE TABLE IF NOT EXISTS `task` (
 -- Structure de la table `task_price`
 --
 
-CREATE TABLE IF NOT EXISTS `task_price` (
+CREATE TABLE `task_price` (
   `task` int(11) NOT NULL,
   `date` datetime NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `notified` tinyint(1) NOT NULL,
-  PRIMARY KEY (`task`,`date`)
+  `notified` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `api_key`
+--
+ALTER TABLE `api_key`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `bundle`
+--
+ALTER TABLE `bundle`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`,`end_date`);
+
+--
+-- Index pour la table `bundle_choice`
+--
+ALTER TABLE `bundle_choice`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bundle_choice_to_bundle` (`bundle`);
+
+--
+-- Index pour la table `bundle_choice_game`
+--
+ALTER TABLE `bundle_choice_game`
+  ADD PRIMARY KEY (`choice`,`game`);
+
+--
+-- Index pour la table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url` (`url`) USING HASH,
+  ADD KEY `url_2` (`url`(768));
+
+--
+-- Index pour la table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product` (`product`);
+
+--
+-- Index pour la table `task_price`
+--
+ALTER TABLE `task_price`
+  ADD PRIMARY KEY (`task`,`date`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `api_key`
+--
+ALTER TABLE `api_key`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `bundle`
+--
+ALTER TABLE `bundle`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `bundle_choice`
+--
+ALTER TABLE `bundle_choice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
@@ -140,3 +232,7 @@ ALTER TABLE `bundle_choice_game`
 ALTER TABLE `task`
   ADD CONSTRAINT `task_to_product` FOREIGN KEY (`product`) REFERENCES `product` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
